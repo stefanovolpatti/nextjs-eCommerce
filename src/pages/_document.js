68038ceac114790694e12161b6d6f545b7cb9066
volp99 <stefanovolpatti@yahoo.it>
2022-04-21 17:12:20 +0200

    create a _document file for snipcart sdk

---
+++ src/pages/_document.js
@@ -0,0 +1,30 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://app.snipcart.com" />
+        <link rel="preconnect" href="https://cdn.snipcart.com" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.snipcart.com/themes/v3.3.3/default/snipcart.css"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+        <script
+          async
+          src="https://cdn.snipcart.com/themes/v3.3.3/default/snipcart.js"
+        ></script>
+        <div
+          hidden
+          id="snipcart"
+          data-api-key="MDg2NWJhN2QtMTNhYi00NTk3LWJiY2EtOWU2NGZlZGJjYzVlNjM3ODYxNDkxOTQyNzcwMTIz"
+          data-config-modal-style="side"
+        ></div>
+      </body>
+    </Html>
+  );
+}
