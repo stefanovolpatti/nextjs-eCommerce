e395f80e722fdc45f69862ef518720cb984b88ea
volp99 <stefanovolpatti@yahoo.it>
2022-04-21 18:35:22 +0200

    query items by featured

--- src/pages/index.js
+++ src/pages/index.js
@@ -97,7 +97,7 @@ export async function getStaticProps() {
           slug
           heroBackground
         }
-        products(first: 4) {
+        products(where: { categories_some: { slug: "featured" } }) {
           id
           name
           price
