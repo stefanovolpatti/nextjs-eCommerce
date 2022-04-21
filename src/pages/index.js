b96b3ae9ca327fc23885476ff071e40c3f825296
volp99 <stefanovolpatti@yahoo.it>
2022-04-21 17:14:13 +0200

    add a product from the homepage to the cart

--- src/pages/index.js
+++ src/pages/index.js
@@ -59,7 +59,16 @@ export default function Home({ home, products }) {
                   </a>
                 </Link>
                 <p>
-                  <Button>Add to Cart</Button>
+                  <Button
+                    className="snipcart-add-item"
+                    data-item-id={product.id}
+                    data-item-price={product.price}
+                    data-item-url={`/products/${product.slug}`}
+                    data-item-image={product.image.url}
+                    data-item-name={product.name}
+                  >
+                    Add to Cart
+                  </Button>
                 </p>
               </li>
             );
@@ -89,6 +98,7 @@ export async function getStaticProps() {
           heroBackground
         }
         products(first: 4) {
+          id
           name
           price
           slug
